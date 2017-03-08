9aa43de286e1205f8578d6501d682841e7cb73c4
ahmetcanbulut <ahmetcanbulut@gmail.com>
2017-03-08 09:33:46 +0200

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -1,15 +1,20 @@
 // Two Sum - LeetCode
 
-public int[] twoSum(int[] numbers, int target) {
-    int[] result = new int[2];
-    Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-    for (int i = 0; i < numbers.length; i++) {
-        if (map.containsKey(target - numbers[i])) {
-            result[1] = i + 1;
-            result[0] = map.get(target - numbers[i]);
-            return result;
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int i =0;
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        int[] result = new int[2];
+        while(true){
+            int current = nums[i];
+            int desired = target - current;
+            if(map.containsKey(desired)){
+                result[0] = map.get(desired);
+                result[1] = i;
+                return result;
+            }
+            map.put(current,i);
+            i++;
         }
-        map.put(numbers[i], i + 1);
     }
-    return result;
 }
